ec4e7e5fb5b02fcb5934e32bbbe35baddd7ab71c
takyorama <takyorama@hotmail.com>
2020-08-29 09:01:50 -0300

    Create ProfessorRepository.php

---
+++ ProfessorRepository.php
@@ -0,0 +1,11 @@
+<?php namespace GestaoEscolar\Repositorios;
+
+class ProfessorRepositorio{
+   private $professor;
+   
+   public fincId($id){}
+   public findName($name){}
+   public Include($Professor $professor){}
+   public Upadate($id, Professor $professor){}
+   public Delete($id){}
+}
