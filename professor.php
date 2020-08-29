68c01ddd9ae69ba98a7ad58c4a209ef5527879cc
takyorama <takyorama@hotmail.com>
2020-08-29 08:57:10 -0300

    Create professor.php
    Classe professor

---
+++ professor.php
@@ -0,0 +1,15 @@
+<?php namespace GestaoEscolar\Dicente;
+
+class Professor{
+   private $codigo;
+   private $id;
+   private $nome;
+  
+   public setCode($code){}
+   public setId($id){}
+   public setName($name){}
+  
+   public getCode(){}
+   public getId(){}
+   public getName(){}
+}
