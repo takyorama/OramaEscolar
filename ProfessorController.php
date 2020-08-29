d2dd962685fa5d0eb022d45bbf825005f7766dfd
takyorama <takyorama@hotmail.com>
2020-08-29 09:05:41 -0300

    Create ProfessorController.php

---
+++ ProfessorController.php
@@ -0,0 +1,10 @@
+<?php namespace GestaoEscolar\Dicente;
+
+class ProfessorController{
+   public function Index(){}
+   public function Create(){}
+   public function Store(Professor $professor){}
+   public function Edit($id){}
+   public function Upadte($id, Professor $professor){}
+   public function Delete($id){}
+?>
